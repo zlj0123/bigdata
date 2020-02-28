8e360630a59d0b22c4dda6953b46afd30bb85fe6
zlj0123 <zlj0123@gmail.com>
2020-02-28 15:17:31 +0800

    watermark and top N

---
+++ flink-quickstart/src/main/java/io/ibigdata/flink/quickstart/java/watermark/MyWaterMark.java
@@ -0,0 +1,38 @@
+package io.ibigdata.flink.quickstart.java.watermark;
+
+import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
+import org.apache.flink.streaming.api.watermark.Watermark;
+
+import javax.annotation.Nullable;
+import java.text.SimpleDateFormat;
+
+public class MyWaterMark implements AssignerWithPeriodicWatermarks<Record> {
+    //定义最大延迟 10s
+    private final long maxOutOfOrderness = 10000L;
+    private long currentMaxTimestamp;
+    private Watermark watermark;
+
+    //将时间戳信息格式化,调试学习
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    @Nullable
+    @Override
+    public Watermark getCurrentWatermark() {
+        watermark = new Watermark(this.currentMaxTimestamp - this.maxOutOfOrderness);
+        return watermark;
+    }
+
+    @Override
+    public long extractTimestamp(Record element, long previousElementTimestamp) {
+        //      获取event中的时间戳
+        long timestamp = element.timestamp;
+        currentMaxTimestamp = Math.max(timestamp, currentMaxTimestamp);
+        //          将所有的时间信息打印
+        System.out.println("currentThreadId:" + Thread.currentThread().getId()
+                + ",key:" + element.name + ",eventTime:[" + element.datetime + "],currentMaxTimestamp:["
+                + sdf.format(currentMaxTimestamp) + "],watermark:["
+                + sdf.format(getCurrentWatermark().getTimestamp()) + "]");
+        //返回event中的时间戳
+        return timestamp;
+    }
+}
---
+++ flink-quickstart/src/main/java/io/ibigdata/flink/quickstart/java/watermark/Record.java
@@ -0,0 +1,7 @@
+package io.ibigdata.flink.quickstart.java.watermark;
+
+public class Record {
+    public String name;
+    public String datetime;
+    public Long timestamp;
+}
