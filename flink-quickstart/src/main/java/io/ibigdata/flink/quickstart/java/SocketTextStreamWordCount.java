22bae7f6f2b4e678141daa53840f96e6ece0c43c
zlj0123 <zlj0123@gmail.com>
2020-03-26 11:09:07 +0800

    update

---
+++ flink-quickstart/src/main/java/io/ibigdata/flink/quickstart/java/SocketTextStreamWordCount.java
@@ -0,0 +1,59 @@
+package io.ibigdata.flink.quickstart.java;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+public class SocketTextStreamWordCount {
+    public static void main(String[] args) throws Exception {
+
+        // the host and the port to connect to
+        final String hostname;
+        final int port;
+        try {
+            final ParameterTool params = ParameterTool.fromArgs(args);
+            hostname = params.has("hostname") ? params.get("hostname") : "localhost";
+            port = params.getInt("port");
+        } catch (Exception e) {
+            System.err.println("No port specified. Please run 'SocketWindowWordCount " +
+                               "--hostname <hostname> --port <port>', where hostname (localhost by default) " +
+                               "and port is the address of the text server");
+            System.err.println("To start a simple text server, run 'netcat -l <port>' and " +
+                               "type the input text into the command line");
+            return;
+        }
+
+        // get the execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // get input data by connecting to the socket
+        DataStream<String> text = env.socketTextStream(hostname, port, "\n");
+
+        // parse the data, group it, window it, and aggregate the counts
+        text.flatMap(new Tokenizer()).setParallelism(1)
+                .keyBy(0)
+                .sum(1).setParallelism(1)
+                .print();
+
+        env.execute("Java WordCount from SocketTextStream Example");
+    }
+
+    public static final class Tokenizer implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            // normalize and split the line
+            String[] tokens = value.toLowerCase().split("\\W+");
+
+            // emit the pairs
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<>(token, 1));
+                }
+            }
+        }
+    }
+}
