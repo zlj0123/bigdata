ac02c7558d5e0f1938501cb40f2bef520db50389
zlj0123 <zlj0123@gmail.com>
2021-09-24 23:29:11 +0800

    cdc

--- flink-cdc/src/main/java/io/ibigdata/flink/cdc/MysqlSourceTest.java
+++ flink-cdc/src/main/java/io/ibigdata/flink/cdc/MysqlSourceTest.java
@@ -13,7 +13,7 @@ public class MysqlSourceTest {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(new Configuration());
 
-        env.setParallelism(4);
+        env.setParallelism(3);
         env.enableCheckpointing(30000);
 
         EnvironmentSettings settings = EnvironmentSettings
@@ -31,18 +31,34 @@ public class MysqlSourceTest {
                         " PRIMARY KEY(id) NOT ENFORCED\n" +
                         ") WITH (\n" +
                         " 'connector' = 'mysql-cdc',\n" +
-                        " 'hostname' = '10.20.30.113',\n" +
-                        " 'port' = '33061',\n" +
-                        " 'scan.incremental.snapshot.chunk.size' = '10',\n" +
+                        " 'hostname' = 'localhost',\n" +
+                        " 'port' = '3306',\n" +
+                        " 'scan.incremental.snapshot.chunk.size' = '8',\n" +
                         " 'username' = 'root',\n" +
-                        " 'password' = 'admin@123',\n" +
-                        " 'database-name' = 'test2',\n" +
-                        " 'table-name' = 'flink_cdc_test2'\n" +
+                        " 'password' = 'Zlj840123',\n" +
+                        " 'database-name' = 'zhanglijun',\n" +
+                        " 'table-name' = 'flink_cdc_test'\n" +
                         ")"
         );
 
-        Table t = tEnv.sqlQuery("SELECT id, age,UPPER(address) FROM mysql_binlog");
-        tEnv.toRetractStream(t, Row.class).print();
+        tEnv.executeSql(
+                "CREATE TABLE mysql_binlog2 (\n" +
+                        " id INT NOT NULL,\n" +
+                        " age INT,\n" +
+                        " address STRING,\n" +
+                        " PRIMARY KEY(id) NOT ENFORCED\n" +
+                        ") WITH (\n" +
+                        " 'connector' = 'jdbc',\n" +
+                        " 'url' = 'jdbc:mysql://localhost:3306/zhanglijun',\n" +
+                        " 'username' = 'root',\n" +
+                        " 'password' = 'Zlj840123',\n" +
+                        " 'table-name' = 'flink_cdc_test_bk'\n" +
+                        ")"
+        );
+
+//        Table t = tEnv.sqlQuery("SELECT id, age,UPPER(address) FROM mysql_binlog");
+//        tEnv.toRetractStream(t, Row.class).print();
+        tEnv.executeSql("insert into mysql_binlog2 select * from mysql_binlog");
         env.execute();
     }
 }
