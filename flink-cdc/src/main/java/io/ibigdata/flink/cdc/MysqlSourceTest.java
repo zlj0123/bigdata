f40b5d51b12a597c560aa570e00e7c93aca8c7e4
zlj0123 <zlj0123@gmail.com>
2021-09-30 18:16:25 +0800

    1.14.0

--- flink-cdc/src/main/java/io/ibigdata/flink/cdc/MysqlSourceTest.java
+++ flink-cdc/src/main/java/io/ibigdata/flink/cdc/MysqlSourceTest.java
@@ -8,7 +8,6 @@ public class MysqlSourceTest {
     public static void main(String[] args) throws Exception {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        //shezhi
         env.setParallelism(3);
         env.enableCheckpointing(30000);
 
