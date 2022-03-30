abfaaae39ec5347bda2d8092d8ed65aa27c7e0b9
zlj0123 <zlj0123@gmail.com>
2022-03-30 17:07:46 +0800

    init
    init

--- hadoop-hdfs/src/main/java/io/ibigdata/hadoop/hdfs/HdfsTest.java
+++ hadoop-hdfs/src/main/java/io/ibigdata/hadoop/hdfs/HdfsTest.java
@@ -2,6 +2,14 @@ package io.ibigdata.hadoop.hdfs;
 
 public class HdfsTest {
     public static void main(String[] args) {
+        String src = "100014|中债收益率曲线|575|中债商业银行同业存单收益率曲线(AAA-)||||260";
 
+        String src1= "123|中国|123|中债||||260";
+
+        String[] strArray = src.split("\\|");
+
+        String[] strArray1 = src1.split("\\|");
+
+        System.out.println();
     }
 }
