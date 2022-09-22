afea5262479510c9af3bd045430c36ab06e94517
zlj0123 <zlj0123@gmail.com>
2022-09-22 09:32:32 +0800

    update hadoop version
    update hadoop version

--- flink-cdc/src/main/java/io/ibigdata/flink/cdc/MySqlBinlogSourceExample.java
+++ flink-cdc/src/main/java/io/ibigdata/flink/cdc/MySqlBinlogSourceExample.java
@@ -1,26 +1,31 @@
 package io.ibigdata.flink.cdc;
 
-import com.ververica.cdc.connectors.mysql.MySqlSource;
-import com.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
+import com.ververica.cdc.connectors.mysql.source.MySqlSource;
+import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
 public class MySqlBinlogSourceExample {
     public static void main(String[] args) throws Exception {
-        SourceFunction<String> sourceFunction = MySqlSource.<String>builder()
+        MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
                 .hostname("localhost")
                 .port(3306)
                 .databaseList("zhanglijun") // monitor all tables under inventory database
                 .tableList("products")
                 .username("root")
                 .password("Zlj840123")
-                .deserializer(new StringDebeziumDeserializationSchema()) // converts SourceRecord to String
+                .deserializer(new JsonDebeziumDeserializationSchema()) // converts SourceRecord to String
                 .build();
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        env.enableCheckpointing(30000);
-        env.addSource(sourceFunction).print().setParallelism(1); // use parallelism 1 for sink to keep message ordering
+        // enable checkpoint
+        env.enableCheckpointing(3000);
+
+        env.fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
+                // set 4 parallel source tasks
+                .setParallelism(4)
+                .print().setParallelism(1); // use parallelism 1 for sink to keep message ordering
 
         env.execute("Print MySQL Snapshot + Binlog");
     }
